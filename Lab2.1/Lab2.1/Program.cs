438db0efcd0c16f7994ecb36498f7218bfb1eb09
Mahfuza <mahfuzasharmili@gmail.com>
2021-02-08 09:07:00 +0600

    lab 2.1

---
+++ Lab2.1/Lab2.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int oddtotal = 0, eventotal = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    eventotal += i;
+                }
+                else
+                {
+                    oddtotal += i;
+                }
+            }
+
+            Console.WriteLine("Even number sum from 1 to 100: " + eventotal);
+            Console.WriteLine("Odd number sum from 1 to 100: " + oddtotal);
+        }
+    }
+}
