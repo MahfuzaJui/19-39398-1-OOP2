2a98fe10207416189dd5f0bc8ab3c048837f7f92
Mahfuza <mahfuzasharmili@gmail.com>
2021-02-08 09:04:14 +0600

    lab2

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+                   
+        {
+            int n = 100;
+
+            Console.Write("Even Numbers: ");
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    if (i == n)
+                    {
+                        Console.Write(i);
+                    }
+                    else
+                    {
+                        Console.Write(i + ", ");
+                    }
+                }
+            }
+            Console.WriteLine("");
+        }
+    }
+}
