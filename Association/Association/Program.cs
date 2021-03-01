2c9fb7653913331052f2818158e8fef520e4ce17
Mahfuza <mahfuzasharmili@gmail.com>
2021-03-01 11:08:57 +0600

    Association final commit

---
+++ Association/Association/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Association
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //One to One
+
+            Student s1 = new Student("19-39398-1", "Jui", 3.9F);
+            Student s2 = new Student("1234", "Mahfuza", 3.85F);
+            Student s3 = new Student("2905", "Sharmili", 3.8F);
+
+            Department d1 = new Department("CS", "Computer Science");
+            Department d2 = new Department("EEE", "Electrical & Electronic Engineering");
+
+            s1.Dept = d1;
+            s2.Dept = d2;
+            s3.Dept = d1;
+
+            s1.ShowDepartment();
+
+            s2.ShowDepartment();
+;
+            s3.ShowDepartment();
+
+         
+            d1.Name = "Computer Science & Engineering";     //change the department name
+
+            s1.ShowDepartment();
+
+            //one to many
+
+            d1.AddStudent(s1);
+            d1.AddStudent(s2);
+
+            d1.PrintStudent();
+        }
+    }
+}
---
+++ Association/Association/Student.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Association
+{
+    class Student
+    {
+        private string id;
+        private string name;
+        private float cgpa;
+
+        private Department dept;
+
+        public Student() { }
+
+        public Student(string id, string name, float cgpa)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Cgpa = cgpa;
+        }
+
+        public string Id
+        {
+            set { this.id = value; }
+            get { return this.id; }
+        }
+
+        public string Name
+        {
+            set { this.name = value; }
+            get { return this.name; }
+
+        }
+
+        public float Cgpa
+        {
+            set { this.cgpa = value; }
+            get { return this.cgpa; }
+        }
+
+        public Department Dept
+        {
+            set { this.dept = value; }
+            get { return this.dept; }
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Student Name: {0}", this.Name);
+            Console.WriteLine("Student ID: {0}", this.Id);
+            Console.WriteLine("CGPA: {0}", this.Cgpa);
+        }
+
+        public void ShowDepartment()
+        {
+            this.ShowInfo();
+            this.Dept.ShowInfo();
+        }
+    }
+}
